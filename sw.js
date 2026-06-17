f4332a16be723047f08781a718427d56501736ae
jmabk6 <jma_iskalia@yahoo.fr>
2026-06-17 22:51:57 +0300

    v93: start_url explicite index.html + SW network-first sans pre-cache

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
-const CACHE = 'voyages-v92';
-const SHELL = [
-  '/Travels-by-JM/',
-  '/Travels-by-JM/index.html',
-  '/Travels-by-JM/apple-touch-icon.png',
-  '/Travels-by-JM/manifest.json'
-];
+const CACHE = 'voyages-v93';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
   self.skipWaiting();
 });
 
@@ -23,13 +16,10 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      }).catch(() => cached);
-    })
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
